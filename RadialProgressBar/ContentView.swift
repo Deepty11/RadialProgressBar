18d21b32a4cfd1195103033841d902214019c32d
Rehnuma Reza (Deepty) <rehnuma.deepty@therapservices.net>
2023-09-20 14:10:28 +0600

    Initial Commit

---
+++ RadialProgressBar/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RadialProgressBar
+//
+//  Created by Rehnuma Reza Deepty on 20/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RadialProgressBar/RadialProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadialProgressBarApp.swift
+//  RadialProgressBar
+//
+//  Created by Rehnuma Reza Deepty on 20/9/23.
+//
+
+import SwiftUI
+
+@main
+struct RadialProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
